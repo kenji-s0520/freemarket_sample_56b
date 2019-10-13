ce5cf781258bfb50459b6f0a30b34e25c4e8bc8e
Mko-dai <mk0829@outlook.jp>
2019-10-13 14:09:57 +0900

    Revert "modification"
    This reverts commit ed9b9c7747c867a0b609558ee9298f5f1fb6b295.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,6 +14,7 @@ CarrierWave.configure do |config|
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
+  
   config.fog_directory  = 'freemarket-56wb'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-56wb'
 end
