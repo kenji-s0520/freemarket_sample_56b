3726ca58fadc2fb3c5a7def9c6fbdda6765e6c91
kenji-s0520 <crimson_impulse@hotmail.co.jp>
2019-10-21 19:37:07 +0900

    mypage_create_Edit_Re2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/mypage' => 'users#mypage'
-  resources :users, oniy: [:index]
+
+  resources :users, only: [:index]
 
 end
