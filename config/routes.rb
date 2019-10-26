be9748f1803ea2aef346b96ca803718083d8f370
ujiietakatoshi <fadeoutinu@gmail.com>
2019-10-26 14:15:20 +0900

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index]
+  root to: "groups#user-login"
 
 end
