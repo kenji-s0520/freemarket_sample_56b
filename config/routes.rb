8412c33249f1ff191bc87b71fe02cee6eec97488
ujiietakatoshi <fadeoutinu@gmail.com>
2019-11-02 12:47:37 +0900

    2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index]
-  root to: "groups#user-login"
+  root to: "users#_user-reg5"
 
 end
