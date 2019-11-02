6a0836e3fdf404a6af3c69fdcd2c1f776e031501
ujiietakatoshi <fadeoutinu@gmail.com>
2019-11-02 15:49:53 +0900

    3

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/users/info' => 'users#info'
   get 'users/:id/card' => 'users#card'
   get '/users/:id/card/create' => 'users#card_create'
-  
+  get '/users/user_reg4'
 
   resources :items, only: [:show]
 end
