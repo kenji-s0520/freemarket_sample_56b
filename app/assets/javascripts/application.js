622dc8512f2081e98a0ef3005b0a50cd3529ac4d
naouezuki <nao6up@gmail.com>
2019-11-04 16:44:07 +0900

    delete %body

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,20 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require_tree .
+//= require_tree
 //= require jquery
+//= require jquery_ujs
+
+$(function() {
+  $('.slider').slick({
+    dots: true,
+    dotsClass: 'slick-dots',
+    cenrterMode: true,
+    centerPadding: 0,
+    adaptiveheight: true,
+    prevArrow: '<img src="/images/carousel-prev.png" class="slideArrow prev">',
+    nextArrow: '<img src="/images/carousel-next.png" class="slideArrow2 next">',
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+});
