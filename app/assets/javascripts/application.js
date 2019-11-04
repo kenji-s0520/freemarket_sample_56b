f34de1cb3d36c9ce279fd688fbab8b108f316988
naouezuki <nao6up@gmail.com>
2019-11-04 12:29:50 +0900

    slide

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,9 +21,10 @@ $(function() {
     dotsClass: 'slick-dots',
     cenrterMode: true,
     centerPadding: 0,
+    adaptiveheight: true,
     prevArrow: '<img src="/images/carousel-prev.png" class="slideArrow prev">',
-    nextArrow: '<img src="/images/carousel-next.png" class="slideArrow next">',
-    autoplay: true,
-    autoplaySpeed: 3000,
+    nextArrow: '<img src="/images/carousel-next.png" class="slideArrow2 next">',
+    // autoplay: true,
+    // autoplaySpeed: 3000,
   });
 });
