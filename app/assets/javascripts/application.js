4130c6ddd8874306fa1d893aa9052838ea65b318
naouezuki <nao6up@gmail.com>
2019-11-04 16:25:53 +0900

    move to toppage.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,17 +14,3 @@
 //= require activestorage
 //= require_tree .
 //= require jquery
-
-$(function() {
-  $('.slider').slick({
-    dots: true,
-    dotsClass: 'slick-dots',
-    cenrterMode: true,
-    centerPadding: 0,
-    adaptiveheight: true,
-    prevArrow: '<img src="/images/carousel-prev.png" class="slideArrow prev">',
-    nextArrow: '<img src="/images/carousel-next.png" class="slideArrow2 next">',
-    autoplay: true,
-    autoplaySpeed: 3000,
-  });
-});
