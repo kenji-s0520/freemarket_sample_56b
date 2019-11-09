8b13c8a77dee5ca0d28d7ad580ac85678cb38a6b
ujiietakatoshi <fadeoutinu@gmail.com>
2019-11-09 14:31:06 +0900

    rspec

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,17 @@ class ItemsController < ApplicationController
   def show
     render "items/items"
   end
+
+  require 'payjp'
+
+  def purchase
+    Payjp.api_key = "" #秘密鍵
+    Payjp::Charge.create(
+      amount: 809, # 決済する値段
+      card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
+      currency: 'jpy'
+    )
+  end
   
 end
 
