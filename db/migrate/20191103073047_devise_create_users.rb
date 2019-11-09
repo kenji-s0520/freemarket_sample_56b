b280922122c8d094f1b12f802aedd6a2c4eb0cf7
Mko-dai <mk0829@outlook.jp>
2019-11-09 13:19:10 +0900

    page mooving setting

--- db/migrate/20191103073047_devise_create_users.rb
+++ db/migrate/20191103073047_devise_create_users.rb
@@ -3,15 +3,16 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      ## Database authenticatable
-      # t.string :nickname,           null: false
-      # t.string :last_name,          null:false
-      # t.string :first_name,         null:false
-      # t.string :last_name_kana,     null:false
-      # t.string :first_name_kana,    null:false
-      # t.string :phone_number,       null: false
+      # Database authenticatable
+      t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :last_name,          null:false
+      t.string :first_name,         null:false
+      t.string :last_name_kana,     null:false
+      t.string :first_name_kana,    null:false
+      t.string :phone_number,       null: false
+      
      
       
 
