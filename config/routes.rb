18e20f80616ded37431fa625527b0a5e5d4721ba
kenji-s0520 <crimson_impulse@hotmail.co.jp>
2019-11-09 16:59:21 +0900

    breadcrumb_create_edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "items#toppage"
 
   resources :users, only: [:index, :edit]
   get '/users/info' => 'users#info'
-  get 'users/:id/card' => 'users#card'
-  get '/users/:id/card/create' => 'users#card_create'
+  get 'users/card' => 'users#card'
+  get '/users/card/create' => 'users#card_create'
 
   resources :items, only: [:show]
 end
