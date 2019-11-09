ef32b5fcafd46d0316158f9dbeba9406d1891220
Mko-dai <mk0829@outlook.jp>
2019-11-09 17:24:33 +0900

    create User model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:birthday,:phone_number,:code,:prefecture,:city_town,:address_number,:bulding,:room_phone_number])
+  end
 
   def production?
     Rails.env.production?
