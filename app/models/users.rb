85fbccfc36a5dbe7699b570b3566e58977184e1b
takaomiyamashita <ty.8479@gmail.com>
2019-11-09 21:11:03 +0900

    deviseにメソッド追加

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,1 +1,5 @@
 has_many :sns_credentials, dependent: :destroy
+
+devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :omniauthable,omniauth_providers: [:facebook]
