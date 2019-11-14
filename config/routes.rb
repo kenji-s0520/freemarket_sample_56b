7a2aa777fcd810b48b89d6a56a24eb4f8f1bd86e
takaomiyamashita <ty.8479@gmail.com>
2019-11-14 19:57:10 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users , controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :edit]
   root to: "items#toppage"
