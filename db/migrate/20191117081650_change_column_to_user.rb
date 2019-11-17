e67b2eba683c2c849593935c843943e120d1d9b3
kenji-s0520 <crimson_impulse@hotmail.co.jp>
2019-11-17 18:07:56 +0900

    DB_Edit_6

--- db/migrate/20191117081650_change_column_to_user.rb
+++ db/migrate/20191117081650_change_column_to_user.rb
@@ -1,35 +1,35 @@
 class ChangeColumnToUser < ActiveRecord::Migration[5.2]
   def change
     def up
-      change_column_default :users, :balance, nil
-    end
-
-    def down
       change_column_default :users, :balance, 0
     end
 
-    def up
-      change_column_default :users, :point, nil
+    def down
+      change_column_default :users, :balance, nil
     end
 
-    def down
+    def up
       change_column_default :users, :point, 0
     end
 
-    def up
-      change_column_default :users, :value, nil
+    def down
+      change_column_default :users, :point, nil
     end
 
-    def down
+    def up
       change_column_default :users, :value, 0
     end
 
+    def down
+      change_column_default :users, :value, nil
+    end
+
     def up
-      change_column_default :users, :payment, nil
+      change_column_default :users, :payment, ""
     end
 
     def down
-      change_column_default :users, :payment, ""
+      change_column_default :users, :payment, nil
     end
 
   end
