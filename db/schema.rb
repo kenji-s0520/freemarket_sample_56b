5e5c9da586652de51f07ab6796480d3815587cc3
jyoshikawa0513 <jawa513@yahoo.co.jp>
2019-11-21 00:27:22 +0900

    add category table to ancestry

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_11_30_093249) do
+ActiveRecord::Schema.define(version: 2019_11_20_150754) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "code", null: false
@@ -42,6 +43,8 @@ ActiveRecord::Schema.define(version: 2019_11_30_093249) do
     t.integer "user_id", null: false
     t.string "card_id", null: false
     t.string "customer_id", null: false
+    t.integer "card_id", null: false
+    t.integer "customer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -65,22 +68,26 @@ ActiveRecord::Schema.define(version: 2019_11_30_093249) do
     t.string "name", null: false
     t.text "description", null: false
 ActiveRecord::Schema.define(version: 2019_11_09_081804) do
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "description", null: false
+    t.text "description", null: false
     t.string "image", null: false
     t.integer "price", null: false
     t.string "size", null: false
-    t.integer "category_id"
+    t.integer "category_id", null: false
     t.integer "brand_id", null: false
     t.string "status", null: false
     t.string "ship_method", null: false
-    t.string "ship_fee", null: false
+    t.integer "ship_fee", null: false
     t.string "ship_person", null: false
     t.string "ship_area", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "seller", null: false
+    t.integer "buyer", null: false
     t.string "ship_days"
   end
 
