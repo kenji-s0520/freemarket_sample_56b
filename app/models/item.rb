0bdb14ed8e24e8d51195ffc33556d9babf34ffdb
jyoshikawa0513 <jawa513@yahoo.co.jp>
2019-11-21 00:33:28 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,3 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  belongs_to :seller
-  belongs_to :buyer
   belongs_to :category
-  belongs_to :brand
-  has_many :likes
-  has_many :trading_comments
-  has_many :item_comments
-  has_many :images, dependent: :destroy
-  
 end
