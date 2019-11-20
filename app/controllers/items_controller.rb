d7974beb8633c567da61286c31fbccfa43bfaf3a
jyoshikawa0513 <jawa513@yahoo.co.jp>
2019-11-21 00:28:10 +0900

    create category_form.js

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,27 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!,except: [:index,:show,:toppage]
   
   def new
+    #セレクトボックスの初期値設定
+    @category_parent_array = ["---"]
+    #データベースから、親カテゴリーのみ抽出し、配列化
+      Category.where(ancestry: nil).each do |parent|
+        @category_parent_array << parent.name
+      end
+    # @category_parents = Category.all.order("ancestry ASC,id ASC").limit(13)
   end
 
   def toppage
+  def get_category_children
+    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+
+  def get_category_grandchildren
+  #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
+  
+  def index
   end
 
   def show
@@ -15,6 +33,5 @@ class ItemsController < ApplicationController
   
   def buy
   end
-
 end
 
