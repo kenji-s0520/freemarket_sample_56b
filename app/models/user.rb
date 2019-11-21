ef698a91c1e482738b895d2b7bf8ae340d23d4f0
takaomiyamashita <ty.8479@gmail.com>
2019-11-21 13:25:55 +0900

    Userモデルにfindメソッド実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,18 @@ class User < ApplicationRecord
   has_many :item_comments
   has_many :trading_comments
   has_many :cards
+
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    auth.info.email,
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+    user
+  end
 end
