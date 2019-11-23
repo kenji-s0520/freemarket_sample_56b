766c9ca2d85ebb356b90015f35559aac950e72f6
naouezuki <nao6up@gmail.com>
2019-11-23 12:23:12 +0900

    slideFIX2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,5 +51,17 @@ $(function() {
     cenrterMode: true,
     centerPadding: 0,
     adaptiveheight: true,
+    mouseover: true,
+    customPaging: function(slick,index) {
+      // スライダーのインデックス番号に対応した画像のsrcを取得
+      var targetImage = slick.$slides.eq(index).find('img').attr('src');
+      // slick-dots > li　の中に上記で取得した画像を設定
+      return '<img src=" ' + targetImage + ' "/>';
   });
 });
+
+$(function() {
+  $('.image-dots').slick({
+
+  })
+})
