9e5a4647d5b895d85d0c06fc2f0452b60ee81589
Mko-dai <mk0829@outlook.jp>
2019-11-23 12:36:41 +0900

    add validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,10 @@
 class Address < ApplicationRecord
-  belongs_to :user,optional: true
+  belongs_to :user
+  
+  #mishima 新規登録時のvalidation
+  validates :user_id, presence: true
+  validates :code, presence: true
+  validates :prefectures, presence: true
+  validates :city_town, presence: true
+  validates :address_number, presence: true
 end
