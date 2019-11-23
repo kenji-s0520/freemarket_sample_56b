d276fbbd68b06ccc7a81f24b8a8de43a182e558b
Mko-dai <mk0829@outlook.jp>
2019-11-23 12:36:41 +0900

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,16 @@ class User < ApplicationRecord
   has_many :item_comments
   has_many :trading_comments
   has_many :cards
+  
+  #mishima 新規登録のvalidation
+  validates :nickname, presence: true
+  validates :email, presence: true,uniqueness: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
+  validates :phone_number, presence: true,uniqueness: true
 end
