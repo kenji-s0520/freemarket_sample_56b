8edd7202c17c170bdfb48bd76d163b457758ca7f
naouezuki <nao6up@gmail.com>
2019-11-24 16:28:49 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_11_24_052959) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -40,11 +39,11 @@ ActiveRecord::Schema.define(version: 2019_11_24_052959) do
   end
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "card_id"
-    t.string "customer_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer "user_id", null: false
+    t.string "card_id", null: false
+    t.string "customer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -89,13 +88,13 @@ ActiveRecord::Schema.define(version: 2019_11_24_052959) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", default: ""
-    t.string "email", default: ""
-    t.string "encrypted_password", default: ""
-    t.string "last_name", default: ""
-    t.string "first_name", default: ""
-    t.string "last_name_kana", default: ""
-    t.string "first_name_kana", default: ""
+    t.string "nickname", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
     t.integer "value"
     t.integer "evaluation"
     t.text "introduction"
