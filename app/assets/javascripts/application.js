cbdbf5c7127a4082a26eee193243b058daaec1cc
naouezuki <nao6up@gmail.com>
2019-11-24 14:56:50 +0900

    challenge.solidthumbnail.migratefix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,37 +16,110 @@
 //= require_tree
 //= require jquery
 //= require jquery_ujs
-$(function() {
-  $('.slider').slick({
-    dots: true,
-    dotsClass: 'slick-dots',
-    cenrterMode: true,
-    centerPadding: 0,
-    adaptiveheight: true,
-    prevArrow: '<img src="images/carousel-prev.png" class="slideArrow prev">',
-    nextArrow: '<img src="images/carousel-next.png" class="slideArrow2 next">',
-    autoplay: true,
-    autoplaySpeed: 3000,
-  });
-});
+// $(function() {
+//   $('.slider').slick({
+//     dots: true,
+//     dotsClass: 'slick-dots',
+//     cenrterMode: true,
+//     centerPadding: 0,
+//     adaptiveheight: true,
+//     prevArrow: '<img src="images/carousel-prev.png" class="slideArrow prev">',
+//     nextArrow: '<img src="images/carousel-next.png" class="slideArrow2 next">',
+//     autoplay: true,
+//     autoplaySpeed: 3000,
+//   });
+// });
 
-$(function() {
-  $('.cp_cssslider').slick({
-    cenrterMode: true,
-    centerPadding: 0,
-    adaptiveheight: true,
+// $(function() {
+//   $(function(){
+//     var cp_slider = "#cp_cssslider"; // スライダー
+//     var imagedots = "#image-dots"; // サムネイル画像アイテム
+//     $(imagedots).each(function(){
+//       var index = 
+//     $(imagedots).index(this);
+//       $(this).attr("data-index", index);
+//     });
+//     $(cp_slider).on('init',function(slick){
+//       var index = $(".slide-item.slick-slide.slick-current").attr("data-slick-index");
+//       $(imagedots+'[data-index="'+index+'"]').addClass("thumbnail-current");
+//     });
+//     $(cp_slider).slick({
+//       arrows: false,
+//       infinite: false //これはつけましょう。
+//     });
+//     $(imagedots).on('click',function(){
+//       var index = $(this).attr("data-index");
+//       $(cp_slider).slick("slickGoTo",index,false);
+//     });
+//     $(cp_slider).on('beforeChange',function(event,slick, currentSlide,nextSlide){
+//       $(imagedots).each(function(){
+//         $(this).removeClass("thumbnail-current");
+//       });
+//       $(imagedots+'[data-index="'+nextSlide+'"]').addClass("thumbnail-current");
+//     });
+//   });
+
+
+// //     $(cp_slider).slick({
+// //       cenrterMode: true,
+// //       centerPadding: 0,
+// //       adaptiveheight: true,
+// //       arrows: false,
+// //       // asNavFor: '.image-dots'
+// //     });
+// //     $(imagedots).slick({
+// //       asNavFor: 'cp_slider',
+// //       slidesToShow: 4,
+// //       focusOnSelect: true,
+// //       infinite: false
+// //     });
+// //     $(imagedots).on( 'mouseenter', function() {
+// //       $( this ).click();
+// //     });
+// //     $(imagedots).on( 'click', 'li', function(e) {
+// //       e.preventDefault();
+// //     });
+// //   });
+// // });
+
+$(function(){
+  var slider = "#cp_cssslider"; // スライダー
+  var thumbnailItem = "#image-dots"; // サムネイル画像アイテム
+  
+  // サムネイル画像アイテムに data-index でindex番号を付与
+  $(thumbnailItem).each(function(){
+   var index = $(thumbnailItem).index(this);
+   $(this).attr("data-index",index);
+  });
+  
+  // スライダー初期化後、カレントのサムネイル画像にクラス「thumbnail-current」を付ける
+  // 「slickスライダー作成」の前にこの記述は書いてください。
+  $(slider).on('init',function(slick){
+   var index = $(".slide-item.slick-slide.slick-current").attr("data-slick-index");
+   $(thumbnailItem+'[data-index="'+index+'"]').addClass("thumbnail-current");
+  });
+
+  //slickスライダー初期化  
+  $(slider).slick({
     arrows: false,
-    asNavFor: '.image-dots'
+    infinite: false //これはつけましょう。
   });
-  $('.image-dots').slick({
-    asNavFor: '.cp_cssslider',
-    slidesToShow: 4,
-    focusOnSelect: true
-  });
-  $( '.image-dots' ).on( 'mouseenter', 'li', function() {
+  //サムネイル画像アイテムをクリックしたときにスライダー切り替え
+  $(thumbnailItem).on('mouseenter',function(){
+    var index = $(this).attr("data-index");
+    $(slider).slick("slickGoTo",index,false);
     $( this ).click();
-  } );
-  $( '.image-dots' ).on( 'click', 'li', function(e) {
+  });
+  
+  $(thumbnailItem).on('click',function(e){
     e.preventDefault();
-  } );
+  });
+
+  //サムネイル画像のカレントを切り替え
+  $(slider).on('beforeChange',function(event, slick, currentSlide, nextSlide){
+    $(thumbnailItem).each(function(){
+      $(this).removeClass("thumbnail-current");
+    });
+    $(thumbnailItem+'[data-index="'+nextSlide+'"]').addClass("thumbnail-current");
+  });
 });
