ae4d0b542def726a44d8e5ca055cd924d07c851d
takaomiyamashita <ty.8479@gmail.com>
2019-11-30 13:27:21 +0900

    Userモデルにfindメソッド実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,18 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
   validates :birthday, presence: true
   validates :phone_number, presence: true,uniqueness: true
+
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    auth.info.email,
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+    user
+  end
 end
