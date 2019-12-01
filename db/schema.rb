851a171c2fd1d4272937c40ea50eea43f89df5e3
jyoshikawa0513 <jawa513@yahoo.co.jp>
2019-12-01 20:44:18 +0900

    edit for creating image_data

--- db/schema.rb
+++ db/schema.rb
@@ -63,16 +63,20 @@ ActiveRecord::Schema.define(version: 2019_12_01_031350) do
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
+    t.string "image", null: false
     t.integer "price", null: false
     t.string "size", null: false
     t.integer "category_id", null: false
     t.integer "brand_id", null: false
     t.string "status", null: false
     t.string "ship_method", null: false
+    t.integer "ship_fee", null: false
     t.string "ship_person", null: false
     t.string "ship_area", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "seller", null: false
+    t.integer "buyer", null: false
     t.string "ship_days"
   end
 
