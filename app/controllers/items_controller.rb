f53d2cf07fa19909bfbe05659e6ca5c31811db4e
ujiietakatoshi <fadeoutinu@gmail.com>
2019-12-01 17:57:03 +0900

    revise

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,15 +21,13 @@ class ItemsController < ApplicationController
   
 
   def buy
-    card = Card.find_by(user_id: "1")
-    # cardの定義を行う
+    card = Card.find_by(user_id: current_user.id)
     if card.blank?
       redirect_to controller: "card", action: "new"
       # カード情報が登録されていなかったら登録画面に遷移する
     else
       @items = Item.find_by(params[id:"1"])
-      Payjp.api_key = 'sk_test_3c6c6f094d2e40b7a314b6c3'
-      # secret.ymlか環境変数に入れる
+      # Payjp.api_key = 'sk_test_3c6c6f094d2e40b7a314b6c3'
       Payjp::Charge.create(
       amount: @items.price, #支払金額
       customer: card.customer_id, #顧客ID
@@ -37,31 +35,10 @@ class ItemsController < ApplicationController
       )
   redirect_to action: 'done' #完了画面に移動
   end
+end
 
   def done
   end
-
-  def pay
-    card = Card.where(user_id: "1").first
-    #あとでcurrent_user.idに変更する。
-    Payjp.api_key = 'sk_test_3c6c6f094d2e40b7a314b6c3'
-    Payjp::Charge.create(
-    @items = Items.find("1"), #支払金額を入力（itemテーブル等に紐づけても良い）
-    :customer => card.customer_id, #顧客ID
-    :currency => 'jpy', #日本円
-  )
-  redirect_to action: 'done' #完了画面に移動
-  end
-end
-
-def done
-end
-
-
-end
-
-def done
-end
 
 end
 
