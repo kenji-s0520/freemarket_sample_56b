4d7f0d61e64f1b4e33fc101cf606c02324cfb4fd
ujiietakatoshi <fadeoutinu@gmail.com>
2019-12-01 19:47:35 +0900

    rspec3

--- spec/controllers/card_controller_spec.rb
+++ spec/controllers/card_controller_spec.rb
@@ -6,19 +6,19 @@ require 'rails_helper'
 #new
 
   #newのビューが表示されるか
-    describe 'GET #user_reg' do
-      it "renders the :user_reg template" do
-        get :user_reg    
-        expect(response).to render_template :user_reg
+    describe 'GET #users/card/create' do
+      it "renders the :new template" do
+        get :create    
+        expect(response).to render_template :create
       end
     end
     
-#user_reg2
+#show
   
-  #user_reg2のビューが表示されるか
-      describe 'GET #user_reg2' do
-        it "renders the :user_reg2 template" do
-          get :user_reg2,params:{user:{"nickname"=>"みしま", "email"=>"mkwkswwdaf@outlook.jp", "password"=>"1111111", "password_confirmation"=>"1111111", "last_name"=>"三島", "first_name"=>"将暉", "last_name_kana"=>"ミシマ", "first_name_kana"=>"コウダイ","birthday"=>{"birthday(1i)"=>"1995","birthday(2i)"=>"08","birthday(3i)"=>"29"}}}
+  #showのビューが表示されるか
+      describe 'GET #shoe' do
+        it "renders the :shoe template" do
+          get :show,params:{card:{"card_id"=>"", "customer_id"=>"", "password_confirmation"=>"1111111", "last_name"=>"三島", "first_name"=>"将暉", "last_name_kana"=>"ミシマ", "first_name_kana"=>"コウダイ","birthday"=>{"birthday(1i)"=>"1995","birthday(2i)"=>"08","birthday(3i)"=>"29"}}}
           expect(response).to render_template :user_reg2
         end
     end
