a5019e17dc106a2c94964775455f607f7a6d7ee2
jyoshikawa0513 <jawa513@yahoo.co.jp>
2019-12-02 23:02:37 +0900

    create spec/models/item_spec.rb

--- Gemfile
+++ Gemfile
@@ -58,6 +58,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   
+  gem 'rspec-rails'
 end
 
 group :production do
--- app/models/Item.rb
+++ app/models/Item.rb
@@ -1,21 +1,20 @@
 class Item < ApplicationRecord
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
+  validates :category_id, presence: true
+  validates :size, presense: true
+  validates :brand_id, null:true
   validates :status, presence: true
-  # validate :validate_status_not_presense
   validates :ship_person, presence: true
   validates :ship_method, presence: true
   validates :ship_area, presence: true
   validates :ship_days, presence: true
   validates :price, presence: true
-
-  has_many :images, dependent: :destroy
+  
+  has_many :images
   accepts_nested_attributes_for :images
-
-  # private
-
-  # def validate_status_not_presense
-  #   errors.add('選択してください') if status.nil?
-  # end
+  
+  
+  
   
 end
