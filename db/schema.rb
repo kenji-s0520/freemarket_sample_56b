df92f3cc4a5efcf27b382ad3f51d6b5a88f97cb0
kenji-s0520 <crimson_impulse@hotmail.co.jp>
2019-12-08 18:16:37 +0900

    DB_edit5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_08_072200) do
+ActiveRecord::Schema.define(version: 2019_12_08_091440) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "code", null: false
@@ -75,11 +75,10 @@ ActiveRecord::Schema.define(version: 2019_12_08_072200) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ship_days"
+    t.string "brand"
     t.integer "brand_id"
-    t.bigint "buyer_id"
-    t.bigint "seller_id"
-    t.index ["buyer_id"], name: "index_items_on_buyer_id"
-    t.index ["seller_id"], name: "index_items_on_seller_id"
+    t.integer "buyer_id"
+    t.integer "seller_id"
   end
 
   create_table "sellers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -123,6 +122,4 @@ ActiveRecord::Schema.define(version: 2019_12_08_072200) do
 
   add_foreign_key "addresses", "users"
   add_foreign_key "images", "items"
-  add_foreign_key "items", "buyers"
-  add_foreign_key "items", "sellers"
 end
