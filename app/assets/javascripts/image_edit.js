34bef31cb7273221a8b64bb71981f2b6a2b73300
naouezuki <nao6up@gmail.com>
2019-12-09 23:25:04 +0900

    addJS

--- app/assets/javascripts/image_edit.js
+++ app/assets/javascripts/image_edit.js
@@ -1,14 +1,29 @@
 $(function(){
-  function readURL(input){
-    if(input.files && input.files[0]){
-      var reader = new FileReader();
-      reader.onload = function(e){
-        $('#img_prv').attr('src',e.target.result);
-      }
-      reader.readAsDataURL(input.files[0]);
-    }
+  //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
+  $('.items-contents__image--drop-box').on('change', 'input[type="file"]', function(e) {
+    var file = e.target.files[0],
+        reader = new FileReader(),
+        $preview = $(".items-contents__image--drop-box--file");
+        t = this;
+   // 画像ファイル以外の場合は何もしない
+   if(file.type.indexOf("image") < 0){
+    return false;
   }
-  $('img_click').change(function(){
-    readURL(this);
-  });
+  // ファイル読み込みが完了した際のイベント登録
+  reader.onload = (function(file) {
+    return function(e) {
+      //既存のプレビューを削除
+      $preview.empty();
+      // .prevewの領域の中にロードした画像を表示するimageタグを追加
+      $preview.append($('.items-contents__image--drop-box--file').attr({
+                src: e.target.result,
+                width: "114px",
+                class: "preview",
+                title: file.name
+            }));
+    };
+  })(file);
+
+  reader.readAsDataURL(file);
+});
 });
