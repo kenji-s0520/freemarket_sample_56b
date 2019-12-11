41b3e45213bccb722db6c66229c5862788f84406
takaomiyamashita <ty.8479@gmail.com>
2019-12-12 08:12:33 +0900

    modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,33 +16,35 @@ class User < ApplicationRecord
   has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "seller_id", class_name: "Item"
   
   #mishima ユーザー新規登録 カラムにvalidationを追加
-  validates :nickname, presence: true
+  # validates :nickname, presence: true
   validates :email, presence: true,uniqueness: true
   validates :password, presence: true,length: {minimum:7},confirmation: true
   validates :password_confirmation, presence: true,length: {minimum:7}
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-  validates :first_name_kana, presence: true,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-  validates :birthday, presence: true
-  validates :phone_number, presence: true,uniqueness: true
+  # validates :last_name, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name_kana, presence: true,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  # validates :first_name_kana, presence: true,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  # validates :birthday, presence: true
+  # validates :phone_number, presence: true,uniqueness: true
 
   validates :nickname, presence: true, length: { maximum: 6 }
 
   #yamashita sns認証
   def self.find_for_oauth(auth)
     user = User.where(uid: auth.uid, provider: auth.provider).first
-
     unless user
+      pass = Devise.friendly_token[0, 20]
       user = User.create(
-        nickname: auth.info.nickname,
+        nickname: 'taka',
         uid:      auth.uid,
         provider: auth.provider,
         email:    auth.info.email,
-        password: Devise.friendly_token[0, 20],
-        first_name: '',
-        last_name: ''
+        password: pass,
+        first_name: 'a',
+        last_name: 'a',
+        password_confirmation: pass
       )
+      # user.save!
     end
     user
   end  
