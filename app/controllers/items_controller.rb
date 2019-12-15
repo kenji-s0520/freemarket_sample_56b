4990e3645cb4716977502a320dcd6b9e965047a6
ujiietakatoshi <fadeoutinu@gmail.com>
2019-12-15 18:54:41 +0900

    delete binding.pry

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -68,7 +68,6 @@ class ItemsController < ApplicationController
   def create
     
     @items = Item.new(item_params)
-    binding.pry
     if @items.save  
     else
       @prefectures = Prefecture.all
