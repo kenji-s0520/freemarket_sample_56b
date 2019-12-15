134ddfb0eeb9a0c4fbb4c854ea5a29af48924fe5
Mko-dai <mk0829@outlook.jp>
2019-12-15 14:35:11 +0900

    update

---
+++ app/assets/javascripts/drop.js
@@ -0,0 +1,39 @@
+function imgPreView(event) {
+  var file = event.target.files[0];
+  var reader = new FileReader();
+  var preview = document.getElementById("preview");
+  var previewImage = document.getElementById("previewImage");
+  
+  if(previewImage != null) {
+    preview.removeChild(previewImage);
+  }
+  reader.onload = function() {
+    var img = document.createElement("img");
+    img.setAttribute("src", reader.result);
+    img.setAttribute("id", "previewImage");
+    preview.appendChild(img);
+    
+  };
+ 
+  reader.readAsDataURL(file);
+}
+
+$(function() {
+  // アップロードするファイルを選択
+  $('input[type=file]').change(function() {
+    $('#clear,#edit,#preview').show();
+    $('#editdefault,#cleardefault,#previewdefault').remove();
+  });
+  
+  $('#clear').click(function() {
+    $('input[type=file]').val('');
+    $(this).hide();
+    previewImage.remove();
+    
+    $("#edit").hide();
+  
+  });
+  $('#cleardefault').click(function() {
+  $('#editdefault,#cleardefault,.fagggg').remove();
+  })
+});
